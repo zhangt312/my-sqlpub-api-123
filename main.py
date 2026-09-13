c3711ad53931780e4283680afa2a71a8028020b6
zhangt312 <zhangt312@163.com>
2026-09-13 22:45:35 +0800

    Limit results for zkxh query to 40 entries

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ QUERIES = {
     "60ksldz": "SELECT * FROM 60K缩量大涨 ",
     "trueslxg": "SELECT * FROM 真缩量新高",
     "cyflweekk":"select * from 超越放量周K",
-    "zkxh":"select * from 周K吸货",
+    "zkxh":"select * from 周K吸货  LIMIT 40",
 }
 
 # ---------- API 接口 ----------
